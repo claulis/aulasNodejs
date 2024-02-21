5380a4a4753ce2977abf69ed65ea71add64e9ad1
claulis <cla_ulis@hotmail.com>
2024-02-21 06:07:09 -0300

    ok

---
+++ Aula 4 - Criar, rer e escrever arquivos/arquivo.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+// Verifica se o arquivo já existe
+fs.access('arquivo.txt', fs.constants.F_OK, (err) => {
+    if (err) {
+        // Arquivo não existe, então cria e escreve nele
+        fs.writeFile('arquivo.txt', 'Olá mundo, meu nome é Claudio.', (err) => {
+            if (err) {
+                console.error('Erro ao criar o arquivo:', err);
+            } else {
+                console.log('O arquivo foi criado e o conteúdo foi escrito.');
+            }
+        });
+    } else{
+        console.log('O arquivo já existe.');
+    }
+});
+
+fs.appendFile('arquivo.txt', '\nMeu nome é Claudio.', (err) => {
+    if (err) {
+        console.error('Erro ao escrever no arquivo:', err);
+    } else {
+        console.log('Conteúdo adicionado ao arquivo.');
+    }
+});
